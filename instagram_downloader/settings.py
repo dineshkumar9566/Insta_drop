a1dc2b4f4af86658255239f1b95e9c9b7a37cfcc
dinesh-vybog <dineshkumar@vybog.com>
2024-08-28 12:32:35 +0530

    static

--- instagram_downloader/settings.py
+++ instagram_downloader/settings.py
@@ -13,7 +13,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
@@ -109,12 +117,12 @@ WSGI_APPLICATION = 'instagram_downloader.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 ALLOWED_HOSTS = [
     '127.0.0.1',        # Allows localhost
@@ -128,7 +136,7 @@ ALLOWED_HOSTS = [
 ]
 
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
