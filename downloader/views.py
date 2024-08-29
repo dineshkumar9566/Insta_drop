796c1876d60b3007cf872bec8182644f74f01633
dinesh-vybog <dineshkumar@vybog.com>
2024-08-29 18:43:10 +0530

    edit

--- downloader/views.py
+++ downloader/views.py
@@ -3,6 +3,27 @@ from rest_framework.decorators import api_view
 from .download_instagram_post import download_instagram_post,download_instagram_video,download_instagram_story  # Ensure this path is correct
 from io import BytesIO
 
+# @api_view(['POST'])
+# def download_post(request):
+#     url = request.data.get('url')
+#     if not url:
+#         return JsonResponse({'error': 'URL parameter is required'}, status=400)
+
+#     # Instagram credentials
+#     ig_username = 'ranjithignored1one@gmail.com'  # Replace with your Instagram username
+#     ig_password = 'Instadrop@12345'  # Replace with your Instagram password
+
+#     try:
+#         # Pass the username and password as arguments to the function
+#         img_io, filename = download_instagram_post(url, ig_username, ig_password)
+#         if img_io:
+#             response = FileResponse(img_io, as_attachment=True, filename=filename)
+#             return response
+#         else:
+#             return JsonResponse({'error': 'Failed to download image'}, status=500)
+#     except Exception as e:
+#         return JsonResponse({'error': str(e)}, status=500)
+
 @api_view(['POST'])
 def download_post(request):
     url = request.data.get('url')
@@ -22,7 +43,10 @@ def download_post(request):
         else:
             return JsonResponse({'error': 'Failed to download image'}, status=500)
     except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+        # Log the exception details
+        print(f"Error occurred while downloading image: {str(e)}")
+        return JsonResponse({'error': 'Failed to download image', 'details': str(e)}, status=500)
+
 
 
 # @api_view(['POST'])
